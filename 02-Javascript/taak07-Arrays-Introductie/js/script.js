8b0e1e61b12f3017532510ed3a7d29f4ab15a24c
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-04-26 12:42:38 +0200

    opdracht af

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,20 @@
+topSeries = ['The 100', 'Green arrow', 'Cobra Kai'];
+console.log('Top 3 series')
+console.log('')
+console.log(topSeries[0]);
+console.log(topSeries[1]);
+console.log(topSeries[2]);
+console.log('')
+topSeries = ['Jurrasic World', 'Karate kid', 'De smurfen'];
+console.log('Top 3 films')
+console.log('')
+console.log(topSeries[0]);
+console.log(topSeries[1]);
+console.log(topSeries[2]);
+console.log('')
+topSeries = ['Fifa 21', 'Rocket league', 'GTA 5'];
+console.log('Top 3 games')
+console.log('')
+console.log(topSeries[0]);
+console.log(topSeries[1]);
+console.log(topSeries[2]);
