37e1af25d9bb159267b3fc662a155cfdca6a3bcc
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-04-26 12:25:57 +0200

    opdracht af

--- 02-Javascript/taak06-Functions-Samenvatting/js/script.js
+++ 02-Javascript/taak06-Functions-Samenvatting/js/script.js
@@ -1,5 +1,6 @@
 var score = 0;
 let getal1;
+let getal2;
 
 function makeRandomNumber(){
     return Math.floor(Math.random()*99+1);
@@ -12,3 +13,92 @@ function showRandomNumber1(){
 }
 
 getal1 = showRandomNumber1();
+
+function showRandomNumber2(){
+    let randomGetal = makeRandomNumber()
+    document.querySelector(".getal2").innerHTML = randomGetal;
+    return randomGetal;
+}
+
+getal2 = showRandomNumber2();
+
+
+/*function geefAntwoord(antwoord){
+    if(getal1 < getal2){
+        
+        
+        if(antwoord == 'lager'){
+            console.log(antwoord);
+            document.body.style.backgroundColor = "green";
+            score++;
+            
+        }
+        else{
+            document.body.style.backgroundColor = "red";
+            score--;
+        }
+        
+        
+    }
+    else{
+        if(antwoord =='hoger'){
+            console.log(antwoord);
+            document.body.style.backgroundColor = "green";
+            score++;
+        }
+        else{
+            document.body.style.backgroundColor = "red";
+            score--;
+        }
+    }*/
+
+    function geefAntwoord(antwoord){
+        if(antwoord == "hoger"){
+            
+            
+            if(getal1 > getal2){
+                console.log(antwoord);
+                document.body.style.backgroundColor = "green";
+                score++;
+                
+            }
+            else{
+                document.body.style.backgroundColor = "red";
+                score--;
+            }
+            
+            
+        }
+        else{
+            if(getal1 < getal2){
+                console.log(antwoord);
+                document.body.style.backgroundColor = "green";
+                score++;
+            }
+            else{
+                document.body.style.backgroundColor = "red";
+                score--;
+            }
+        }
+    
+    document.getElementById("score").innerHTML = score;
+    function makeRandomNumber(){
+        return Math.floor(Math.random()*99+1);
+    }
+    
+    function showRandomNumber1(){
+        let randomGetal = makeRandomNumber()
+        document.querySelector(".getal1").innerHTML = randomGetal;
+        return randomGetal;
+    }
+    
+    getal1 = showRandomNumber1();
+    
+    function showRandomNumber2(){
+        let randomGetal = makeRandomNumber()
+        document.querySelector(".getal2").innerHTML = randomGetal;
+        return randomGetal;
+    }
+    
+    getal2 = showRandomNumber2();
+}
