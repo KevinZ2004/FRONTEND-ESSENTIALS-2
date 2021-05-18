4dea055d2cf398ef77541ee2d8804e3bc7d98cd9
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-05-18 11:40:47 +0200

    opdracht af

--- 02-Javascript/taak13-Loops-Fibonacci/js/script.js
+++ 02-Javascript/taak13-Loops-Fibonacci/js/script.js
@@ -1,3 +1,17 @@
-var fibonacci = [0,1,1,2,3,5,8,13,21,34,55];
+function outputLetters(str) {
+    for (let i = 0; i < str.length; i++) {
+        const element = array[i];
+    }
+}
+
+var askString = prompt("typ een iets met 5 letters");
+
+console.log(askString.length);
+
+console.log(askString[0]);
+console.log(askString[1]);
+console.log(askString[2]);
+console.log(askString[3]);
+console.log(askString[4]);
    
 
